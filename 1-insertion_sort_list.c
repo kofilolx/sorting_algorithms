7f20c6f147fca08f745773b23863054f7b73c724
kofilolx <kofyamando@gmail.com>
2024-02-21 05:49:37 -0800

    Sorting - Insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,15 +9,14 @@
  */
 void swap_node(listint_t *a, listint_t *b)
 {
-        if (a->prev)
-                a->prev->next = b;
-        if (b->next)
-                b->next->prev = a;
-        a->next = b->next;
-        b->prev = a->prev;
-        a->prev = b;
-        b->next = a;
-
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
 }
 /**
  * insertion_sort_list - insertion sorts a doubly-linked list
